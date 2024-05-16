27ec8d2c063381ca38ce20bc82e997a342f762f8
Jeff Capone <jeffcap1@gmail.com>
2024-05-16 01:35:27 -0400

    :sparkles: feat: new bg images and changes to bg settings

--- background.lua
+++ background.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 local mux = wezterm.mux
 
 local module = {}
@@ -7,12 +7,12 @@ local show_bg = true
 local img = nil
 
 local function get_random_bgimg()
-  local dir = wezterm.home_dir .. '/.config/wezterm/art'
+  local dir = wezterm.home_dir .. "/.config/wezterm/art"
   local res = io.popen('find "' .. dir .. '" -type f') or {}
 
   local imgs = {}
   for file in res:lines() do
-    if file:match('.DS_Store$') then
+    if file:match(".DS_Store$") then
       goto continue
     end
     table.insert(imgs, file)
@@ -21,13 +21,13 @@ local function get_random_bgimg()
 
   math.randomseed(os.time())
   local img = imgs[math.random(#imgs)]
-  wezterm.log_info('Selected background image: ' .. img)
+  wezterm.log_info("Selected background image: " .. img)
   return img
 end
 
 local function set_background(window, img)
   local overrides = window:get_config_overrides() or {}
-  local bg_img = img or wezterm.home_dir .. '/.config/wezterm/art/master-sword-botw.jpg'
+  local bg_img = img or wezterm.home_dir .. "/.config/wezterm/art/master-sword-botw.jpg"
 
   -- The art is a bit too bright and colorful to be useful as a backdrop
   -- for text, so we're going to dim it down to 10% of its normal brightness
@@ -35,15 +35,15 @@ local function set_background(window, img)
 
   local base_color_bg = {
     source = {
-      Color = '#24273a'
+      Color = "#24273a",
     },
     width = "100%",
     height = "100%",
-    opacity = 0.9
+    opacity = 0.85,
   }
 
   if show_bg then
-    overrides.text_background_opacity = .9
+    overrides.text_background_opacity = 0.9
     overrides.background = {
       -- This is the deepest/back-most layer. It will be rendered first
       base_color_bg,
@@ -54,32 +54,32 @@ local function set_background(window, img)
         },
         hsb = dimmer,
         opacity = 0.5,
-        horizontal_align = 'Center'
+        horizontal_align = "Center",
       },
     }
   else
-    overrides.text_background_opacity = .9
-    base_color_bg.opacity = .9
+    overrides.text_background_opacity = 0.9
+    base_color_bg.opacity = 0.95
     overrides.background = {
-      base_color_bg
+      base_color_bg,
     }
   end
 
   window:set_config_overrides(overrides)
 end
 
-wezterm.on('set-random-bgimg', function(window)
+wezterm.on("set-random-bgimg", function(window)
   show_bg = true
   img = get_random_bgimg()
   set_background(window, img)
 end)
 
-wezterm.on('toggle-bgimg-visible', function(window)
+wezterm.on("toggle-bgimg-visible", function(window)
   if img ~= nil then
     show_bg = not show_bg
     set_background(window, img)
   else
-    wezterm.log_info('No background image is set, skipping showing image')
+    wezterm.log_info("No background image is set, skipping showing image")
   end
 end)
 
@@ -88,24 +88,24 @@ function module.apply_to_config(config)
     {
       key = "b",
       mods = "CMD|SHIFT",
-      action = wezterm.action.EmitEvent 'toggle-bgimg-visible',
+      action = wezterm.action.EmitEvent("toggle-bgimg-visible"),
     },
     {
       key = "r",
       mods = "CMD|SHIFT",
-      action = wezterm.action.EmitEvent 'set-random-bgimg',
-    }
+      action = wezterm.action.EmitEvent("set-random-bgimg"),
+    },
   }
 end
 
-wezterm.on('gui-attached', function()
+wezterm.on("gui-attached", function()
   -- maximize all displayed windows on startup and set background image
   local workspace = mux.get_active_workspace()
   for _, window in ipairs(mux.all_windows()) do
     if window:get_workspace() == workspace then
       local gui_window = window:gui_window()
       gui_window:maximize()
-      gui_window:perform_action(wezterm.action.EmitEvent 'set-random-bgimg', window:active_pane())
+      gui_window:perform_action(wezterm.action.EmitEvent("set-random-bgimg"), window:active_pane())
     end
   end
 end)
