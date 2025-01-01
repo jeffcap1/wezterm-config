0df76a16dbf3d5952975cb199670811712a785cf
Jeff Capone <jeffcap1@gmail.com>
2025-01-01 16:44:40 -0500

    :sparkles: feat: Adjusting backround opacity and blur

--- background.lua
+++ background.lua
@@ -7,96 +7,95 @@ local show_bg = true
 local img = nil
 
 local function get_random_bgimg()
-  local dir = wezterm.home_dir .. "/.config/wezterm/art"
-  local res = io.popen('find "' .. dir .. '" -type f') or {}
+	local dir = wezterm.home_dir .. "/.config/wezterm/art"
+	local res = io.popen('find "' .. dir .. '" -type f') or {}
 
-  local imgs = {}
-  for file in res:lines() do
-    if file:match(".DS_Store$") then
-      goto continue
-    end
-    table.insert(imgs, file)
-    ::continue::
-  end
+	local imgs = {}
+	for file in res:lines() do
+		if file:match(".DS_Store$") then
+			goto continue
+		end
+		table.insert(imgs, file)
+		::continue::
+	end
 
-  math.randomseed(os.time())
-  local img = imgs[math.random(#imgs)]
-  wezterm.log_info("Selected background image: " .. img)
-  return img
+	math.randomseed(os.time())
+	local img = imgs[math.random(#imgs)]
+	wezterm.log_info("Selected background image: " .. img)
+	return img
 end
 
 local function set_background(window, img)
-  local overrides = window:get_config_overrides() or {}
-  local bg_img = img or wezterm.home_dir .. "/.config/wezterm/art/master-sword-botw.jpg"
+	local overrides = window:get_config_overrides() or {}
+	local bg_img = img or wezterm.home_dir .. "/.config/wezterm/art/master-sword-botw.jpg"
 
-  -- The art is a bit too bright and colorful to be useful as a backdrop
-  -- for text, so we're going to dim it down to 10% of its normal brightness
-  local dimmer = { brightness = 0.1 }
+	-- The art is a bit too bright and colorful to be useful as a backdrop
+	-- for text, so we're going to dim it down to 10% of its normal brightness
+	local dimmer = { brightness = 0.1 }
 
-  local base_color_bg = {
-    source = {
-      Color = "#24273a",
-    },
-    width = "100%",
-    height = "100%",
-    opacity = 0.85,
-  }
+	local base_color_bg = {
+		source = {
+			Color = "#24273a",
+		},
+		width = "100%",
+		height = "100%",
+		opacity = 0.85,
+	}
 
-  if show_bg then
-    overrides.text_background_opacity = 0.9
-    overrides.background = {
-      -- This is the deepest/back-most layer. It will be rendered first
-      base_color_bg,
-      -- Subsequent layers are rendered over the top of each other
-      {
-        source = {
-          File = bg_img,
-        },
-        hsb = dimmer,
-        opacity = 0.5,
-        horizontal_align = "Center",
-      },
-    }
-  else
-    overrides.text_background_opacity = 0.9
-    base_color_bg.opacity = 0.95
-    overrides.background = {
-      base_color_bg,
-    }
-  end
+	if show_bg then
+		overrides.text_background_opacity = 0.9
+		overrides.background = {
+			-- This is the deepest/back-most layer. It will be rendered first
+			base_color_bg,
+			-- Subsequent layers are rendered over the top of each other
+			{
+				source = {
+					File = bg_img,
+				},
+				hsb = dimmer,
+				opacity = 0.5,
+				horizontal_align = "Center",
+			},
+		}
+	else
+		overrides.text_background_opacity = 0.9
+		base_color_bg.opacity = 0.85
+		overrides.background = {
+			base_color_bg,
+		}
+	end
 
-  window:set_config_overrides(overrides)
+	window:set_config_overrides(overrides)
 end
 
 wezterm.on("set-random-bgimg", function(window)
-  show_bg = true
-  img = get_random_bgimg()
-  set_background(window, img)
+	show_bg = true
+	img = get_random_bgimg()
+	set_background(window, img)
 end)
 
 wezterm.on("toggle-bgimg-visible", function(window)
-  if img ~= nil then
-    show_bg = not show_bg
-    set_background(window, img)
-  else
-    wezterm.log_info("No background image is set, skipping showing image")
-  end
+	if img ~= nil then
+		show_bg = not show_bg
+		set_background(window, img)
+	else
+		wezterm.log_info("No background image is set, skipping showing image")
+	end
 end)
 
 function module.apply_to_config(config)
-  config.keys = {
-    {
-      key = "b",
-      mods = "CMD|SHIFT",
-      action = wezterm.action.EmitEvent("toggle-bgimg-visible"),
-    },
-    {
-      key = "r",
-      mods = "CMD|SHIFT",
-      action = wezterm.action.EmitEvent("set-random-bgimg"),
-    },
-  }
+	config.keys = {
+		{
+			key = "b",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.EmitEvent("toggle-bgimg-visible"),
+		},
+		{
+			key = "r",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.EmitEvent("set-random-bgimg"),
+		},
+	}
 end
-
 
 return module
--- theme.lua
+++ theme.lua
@@ -4,43 +4,43 @@ local wezterm = require("wezterm")
 local module = {}
 
 local active_tab = {
-  bg_color = "#bc93f9",
-  fg_color = "#191724",
+	bg_color = "#bc93f9",
+	fg_color = "#191724",
 }
 
 local inactive_tab = {
-  bg_color = "#191724",
-  fg_color = "#6e6a86",
+	bg_color = "#191724",
+	fg_color = "#6e6a86",
 }
 
 function module.apply_to_config(config)
-  config.color_scheme = "Catppuccin Macchiato"
+	config.color_scheme = "Catppuccin Macchiato"
 
-  -- command palette adjustments
-  config.command_palette_bg_color = "#151521"
-  config.command_palette_fg_color = "#b5b9c2"
+	-- command palette adjustments
+	config.command_palette_bg_color = "#151521"
+	config.command_palette_fg_color = "#b5b9c2"
 
-  -- window adjustments
-  config.window_background_opacity = 0.95
-  config.macos_window_background_blur = 15
+	-- window adjustments
+	config.window_background_opacity = 0.95
+	config.macos_window_background_blur = 20
 
-  -- window color overrides
-  config.window_frame = {
-    active_titlebar_bg = "#191724",
-    inactive_titlebar_bg = "#191724",
-  }
+	-- window color overrides
+	config.window_frame = {
+		active_titlebar_bg = "#191724",
+		inactive_titlebar_bg = "#191724",
+	}
 
-  config.colors = {
-    tab_bar = {
-      background = "#191724",
-      active_tab = active_tab,
-      inactive_tab = inactive_tab,
-      inactive_tab_hover = active_tab,
-      new_tab = inactive_tab,
-      new_tab_hover = active_tab,
-      inactive_tab_edge = "#6e6a86", -- (Fancy tab bar only)
-    },
-  }
+	config.colors = {
+		tab_bar = {
+			background = "#191724",
+			active_tab = active_tab,
+			inactive_tab = inactive_tab,
+			inactive_tab_hover = active_tab,
+			new_tab = inactive_tab,
+			new_tab_hover = active_tab,
+			inactive_tab_edge = "#6e6a86", -- (Fancy tab bar only)
+		},
+	}
 end
 
 -- return our module table
